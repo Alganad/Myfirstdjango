750234de0f93aebca446e06e1a46001139f7b98e
Alganad <96503379+Alganad@users.noreply.github.com>
2022-03-25 02:27:13 +0300

    Initial commit

---
+++ Base/api/views.py
@@ -0,0 +1,26 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from Base.models import Room
+from .serializers import Roomserializer
+from Base.api import serializers
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'GET /api',
+        'GET /api/rooms', 
+        'GET /api/rooms/:id'
+    ] 
+    return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request ):
+    rooms = Room.objects.all()
+    serializer = Roomserializer(rooms, many=True)
+    return Response(serializer.data)    
+
+@api_view(['GET'])
+def getRoom(request, pk ):
+    room = Room.objects.get(id=pk)
+    serializer = Roomserializer(room, many=False)
+    return Response(serializer.data)    
